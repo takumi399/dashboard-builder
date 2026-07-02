b99768304f6d057da86b4695f814abf6fe9d8953
Developer <developer@dashboard-builder.local>
2026-07-02 21:58:05 +0800

    fix: 用Antd App组件包裹以正确提供message/modal上下文

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, App as AntdApp } from 'antd';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardListPage from './pages/DashboardListPage';
@@ -11,18 +11,20 @@ import DashboardViewPage from './pages/DashboardViewPage';
 const App: React.FC = () => {
   return (
     <ConfigProvider theme={{ token: { colorPrimary: '#1677ff' } }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboards" element={<DashboardListPage />} />
-          <Route path="/datasources" element={<DataSourcePage />} />
-          <Route path="/editor/:id" element={<DashboardEditorPage />} />
-          <Route path="/view/:token" element={<DashboardViewPage />} />
-          <Route path="/" element={<Navigate to="/dashboards" replace />} />
-          <Route path="*" element={<Navigate to="/dashboards" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <AntdApp>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboards" element={<DashboardListPage />} />
+            <Route path="/datasources" element={<DataSourcePage />} />
+            <Route path="/editor/:id" element={<DashboardEditorPage />} />
+            <Route path="/view/:token" element={<DashboardViewPage />} />
+            <Route path="/" element={<Navigate to="/dashboards" replace />} />
+            <Route path="*" element={<Navigate to="/dashboards" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </AntdApp>
     </ConfigProvider>
   );
 };
